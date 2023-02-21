0df66d375dc9948c840be7ae1571986c19bd8595
vallenov <a2lenov@mail.ru>
2023-02-22 00:49:08 +0300

    Доработки классов

--- moex/moex.py
+++ moex/moex.py
@@ -1,54 +1,56 @@
 import os
 
+import settings
 
-class MultiMarketItem:
 
-    def __init__(self, url, name):
+class MOEXMultiObject:
+
+    def __init__(self, url: str, name: str = ''):
         self.url = os.path.join(url, name)
 
-    def securities(self, return_type):
+    def securities(self, return_type=settings.RETURN_TYPE):
         return os.path.join(self.url, f'securities.{return_type}')
 
 
-class Market:
+class MOEXObject:
+    url = ''
+    valid_items = []
+
+    def __init__(self, url, child_item_cls: type):
+        if not hasattr(self, 'url') or not hasattr(self, 'valid_items'):
+            raise NotImplementedError
+        self.url = os.path.join(url, self.url)
+        for item in self.valid_items:
+            setattr(self, item, child_item_cls(self.url, item))
+
+    def securities(self, return_type=settings.RETURN_TYPE):
+        return os.path.join(self.url, f'securities.{return_type}')
+
+
+class MultiMarketItem(MOEXMultiObject):
+    ...
+
+
+class Market(MOEXObject):
     url = 'markets'
-    valid_markets = ['shares', 'bonds', 'index']
-
-    def __init__(self, url):
-        self.url = os.path.join(url, Market.url)
-        for market in Market.valid_markets:
-            setattr(self, market, MultiMarketItem(self.url, market))
-
-    def securities(self, return_type):
-        return os.path.join(self.url, f'securities.{return_type}')
+    valid_items = ['shares', 'bonds', 'index']
 
 
-class MultiEngineItem:
+class MultiEngineItem(MOEXMultiObject):
 
     def __init__(self, url, name):
-        self.url = os.path.join(url, name)
-        self.markets = Market(self.url)
-
-    def securities(self, return_type):
-        return os.path.join(self.url, f'securities.{return_type}')
+        super().__init__(url, name)
+        self.markets = Market(self.url, child_item_cls=MultiMarketItem)
 
 
-class Engine:
+class Engine(MOEXObject):
     url = 'engines'
-    valid_engines = ['stock', 'currency', 'futures']
-
-    def __init__(self, url):
-        self.url = os.path.join(url, Engine.url)
-        for engine in Engine.valid_engines:
-            setattr(self, engine, MultiEngineItem(self.url, engine))
-
-    def securities(self, return_type):
-        return os.path.join(self.url, f'securities.{return_type}')
+    valid_items = ['stock', 'currency', 'futures']
 
 
 class MOEX:
     url = 'https://iss.moex.com/iss'
-    engines = Engine(url)
+    engines = Engine(url, child_item_cls=MultiEngineItem)
 
 
 m = MOEX()
