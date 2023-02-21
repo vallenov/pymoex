55f5257591342a52fd3cd98ee7652b2383e374fb
vallenov <a2lenov@mail.ru>
2023-02-22 01:49:07 +0300

    Переработка классов

--- moex/moex.py
+++ moex/moex.py
@@ -1,30 +1,40 @@
 import os
+import re
 
 import settings
 
 
-class MOEXMultiObject:
+class MOEXBase:
+
+    def securities(
+            self,
+            return_type=settings.RETURN_TYPE,
+            date=None
+    ):
+        url = os.path.join(self.url, f'securities.{return_type}')
+        if date:
+            if not re.match('^\d{4}-\d{2}-\d{2}$', date):
+                raise ValueError('The right format is: YYYY-MM-DD')
+            url += f'?date={date}'
+        return url
+
+
+class MOEXMultiObject(MOEXBase):
 
     def __init__(self, url: str, name: str = ''):
         self.url = os.path.join(url, name)
 
-    def securities(self, return_type=settings.RETURN_TYPE):
-        return os.path.join(self.url, f'securities.{return_type}')
 
-
-class MOEXObject:
+class MOEXObject(MOEXBase):
     url = ''
     valid_items = []
 
-    def __init__(self, url, child_item_cls: type):
-        if not self.url or not self.valid_items:
+    def __init__(self, url, child_item_cls: type = None):
+        if not self.url:
             raise NotImplementedError
         self.url = os.path.join(url, self.url)
         for item in self.valid_items:
             setattr(self, item, child_item_cls(self.url, item))
-
-    def securities(self, return_type=settings.RETURN_TYPE):
-        return os.path.join(self.url, f'securities.{return_type}')
 
 
 class MultiMarketItem(MOEXMultiObject):
@@ -48,14 +58,27 @@ class Engine(MOEXObject):
     valid_items = ['stock', 'currency', 'futures']
 
 
+class History(MOEXObject):
+    url = 'history'
+
+    def __init__(self, url):
+        super().__init__(url)
+        self.engines = Engine(self.url, child_item_cls=MultiEngineItem)
+
+
 class MOEX:
     url = 'https://iss.moex.com/iss'
     engines = Engine(url, child_item_cls=MultiEngineItem)
+    history = History(url)
 
 
 m = MOEX()
-print(m.engines.securities(return_type='json'))
-print(m.engines.stock.securities(return_type='json'))
-print(m.engines.stock.markets.securities(return_type='json'))
-print(m.engines.stock.markets.shares.securities(return_type='json'))
+print(m.engines.securities())
+print(m.engines.stock.securities())
+print(m.engines.stock.markets.securities(date='2022-03-12'))
+print(m.engines.stock.markets.shares.securities())
+print(m.history.engines.securities())
+print(m.history.engines.stock.securities())
+print(m.history.engines.stock.markets.securities())
+print(m.history.engines.stock.markets.shares.securities(date='2022-03-12'))
 
