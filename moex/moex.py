65c861d9f0b8fbd0fe527f54a5a15b0cfb2c471a
vallenov <a2lenov@mail.ru>
2023-02-23 01:50:26 +0300

    Добавлены запросы по сгенерированным линкам

--- moex/moex.py
+++ moex/moex.py
@@ -1,16 +1,49 @@
 import os
 import re
+import json
 
 import settings
+from helpers import regular_request
 
 
 class MOEXBase:
     url = ''
 
-    def __init__(self, url):
+    def __init__(self, url=''):
         if not self.url:
-            raise ValueError('url is need to be fit')
+            raise ValueError('url is empty')
         self.url = os.path.join(url, self.url)
+
+    def __repr__(self):
+        return f'{self.__class__}'
+
+    @staticmethod
+    def _for_normal_dict(text: str) -> dict:
+        normal_dict = {}
+        raw_dict: dict = json.loads(text)
+        for key, val in raw_dict.items():
+            if key == 'metadata':
+                continue
+            low_vals = [low.lower() for low in val['columns']]
+            normal_dict[key] = []
+            for data in val['data']:
+                row = dict(zip(low_vals, data))
+                normal_dict[key].append(row)
+        return normal_dict
+
+    def _make_request(self, url):
+        result = regular_request(url)
+        if result.status_code == 200:
+            data = self._for_normal_dict(result.text)
+        else:
+            data = {}
+        return {
+            'data': data,
+            'metadata': {
+                'code': result.status_code,
+                'message': result.reason
+            }
+        }
 
     def securities(
             self,
@@ -22,7 +55,7 @@ class MOEXBase:
             if not re.match(r'^\d{4}-\d{2}-\d{2}$', date):
                 raise ValueError('The right format is: YYYY-MM-DD')
             url += f'?date={date}'
-        return url
+        return self._make_request(url)
 
 
 class MarketShares(MOEXBase):
@@ -89,13 +122,14 @@ class History(MOEXBase):
         self.engines = Engines(self.url)
 
 
-class MOEX:
+class MOEX(MOEXBase):
     url = 'https://iss.moex.com/iss'
     engines = Engines(url)
     history = History(url)
 
     def index(self, return_type='json'):
-        return os.path.join(self.url, f'index.{return_type}')
+        url = os.path.join(self.url, f'index.{return_type}')
+        return self._make_request(url)
 
 
 moex = MOEX()
